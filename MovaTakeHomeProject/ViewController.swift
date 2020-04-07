b2ee91d2d63bc72f0734432ca76212bc04981b2d
Sergey Yelmanov <sergey.yelmanov@cygility.com>
2020-04-07 17:09:51 +0300

    Initial Commit

---
+++ MovaTakeHomeProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovaTakeHomeProject
+//
+//  Created by Sergey Yelmanov on 07.04.2020.
+//  Copyright © 2020 Sergey Yelmanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
